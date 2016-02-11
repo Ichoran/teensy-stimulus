510a458d6cd71d6bd4cbdce73ec27a7ca60687e9
Rex Kerr <ichoran@gmail.com>
2016-02-11 15:09:22 -0800

    Working through state machine logic for stimulator.

--- teensy-stim/teensy-stim.ino
+++ teensy-stim/teensy-stim.ino
@@ -12,6 +12,7 @@ byte* eepsig = (byte*)"stim1.0 ";
 int lo = LOW;
 int hi = HIGH;
 
+#define LED_PIN 13
 #define PROT 254
 #define CHAN 25
 #define DIG 24
@@ -27,26 +28,53 @@ struct Dura {
   int u;
 };
 
-struct State {
-  byte protocol;
-  byte running;
-  byte stimulated;
-  byte pulsed;
+struct Protocol {
+  Dura t;
   Dura d;
-  Dura i;
   Dura y;
   Dura n;
   Dura p;
   Dura q;
+  byte i;
+  byte s;
+  byte next;
+  byte unused0;
+}
+
+struct State {
+  Dura t;
+  Dura yn;
+  Dura pq;
+  byte i;
+  byte s;
+  byte protocol;
+  byte runlevel;
+  byte pin;
+  byte unused0;
+  byte unused1;
+  byte unused2;
 };
 
-State protocols[PROT];
+int proto_i = 0;
+Protocol protocols[PROT];
+
 State channels[CHAN];
+
+int errormsgN = 12;
+byte errormsg[12];
+
+char buf[63];
 
 void read_my_eeprom() {
   // Make sure the EEPROM corresponds to the expected device
   int i = 0;
-  for (; i < eepsigN; i++) if (EEPROM.read(i) != eepsig[i]) return;
+  for (; i < eepsigN; i++) if (EEPROM.read(i) != eepsig[i]) {
+    char *msg = "wrong-ver"
+    runlevel = 255;
+    for (int j = 0; j < 9; j++) errormsg[j] = msg[j];
+    for (; j < errormsgN; j++) errormsg[j] = ' ';
+    return;
+  }
 
   // Read wave number if it's there
   if (EEPROM.read(i++) != 0) {
@@ -72,33 +100,102 @@ void read_my_eeprom() {
   }
 }
 
-void set_digi() {
-}
-
 void init_pins() {
   if (waveN > 0) analogWriteResolution(12);
+  pinMode(LED_PIN, OUTPUT);
   for (int i = 0; i < digiN; i++) pinMode(digi[i], OUTPUT);
 }
 
-int fix;
+int tick;
+Dura global_clock;
+volatile int running;
+volatile int errorcode;
+volatile bool finished;
 
-const int led = 13;
+void advance(Dura *x, int us) {
+  x->u += us;
+  while (x->u >= 1000000) { x->u -= 1000000; x->s += 1; }
+}
+
+void advance_from(Dura *x, Dura v) {
+  x->u += v.u;
+  x->s += v.s;
+  while (x->u >= 1000000) { x->u -= 1000000; x->s += 1; }
+}
+
+void diminish(Dura *x, int us) {
+  x->u -= us;
+  while (x->u < 0) { x->u += 1000000; x->s -= 1; }
+}
+
+void diminish_by(Dura *x, Dura v) {
+  x->u -= v.u;
+  x->s -= v.s;
+  while (x->u < 0) { x->u += 1000000; x->s -= 1; }
+}
 
 // Setup runs once at reset / power on
 void setup() {
   read_my_eeprom();
+  init_pins();
   Serial.begin(115200);
   ARM_DEMCR |= ARM_DEMCR_TRCENA;
   ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA;
-  pinMode(led, OUTPUT);
   fix = micros();
   for (int i = 0; i < PROT; i++) protocols[i].protocol = 255;
   for (int j = 0; j < CHAN; j++) channels[j].protocol = 255;
+  running = 0;
+  errorcode = 0;
+  finished = false;
 }
 
 // Loop runs forever
 void loop() {
-  int r0 = ARM_DWT_CYCCNT;
+  if (running > 0 && errorcode == 0) {
+    int now = micros();
+    int delta = now - tick;
+    tick = now;
+    advance(&global_clock, delta);
+    for (int i = 0; i < digiN; i++) {
+      if (channels[i].runlevel > 0) {
+        Channel *ci = &channels[i];
+        diminish(&(ci->t), delta);
+        if (ci->t.s < 0) {
+          if (ci->runlevel == 3) turn_off(ci);
+          ci->runlevel = 0;
+          runlevel -= 1;
+          continue;   // CONTINUE WITH NEXT CHANNEL, THIS ONE IS DONE
+        }
+        else {
+          diminish(&(ci->yn), delta);
+          if (ci->yn.s < 0) {
+            bool flipped = false;
+            bool on = (ci->runlevel > 1);
+            while (ci->yn.s < 0) {
+              advance_from(&(ci->yn), on ? protocol[ci->protocol].n : protocol[ci->protocol].y);
+              flipped = !flipped;
+              on = !on;
+            }
+            if (!on) {
+              if (flipped) { 
+                if (ci->runlevel == 3) turn_off(ci);
+                ci->runlevel = 1;
+              }
+            }
+            else {
+              // TODO
+            }
+          }
+          else {
+            // TODO
+          }
+        }
+      }
+    }
+    if (running == 0 || errorcode != 0) finished = true;
+  }
+
+
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   int m = micros();
   int n = 100000;
